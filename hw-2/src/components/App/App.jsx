69ce12b72b7570b2f7b2a95d439466f19c555957
Ruslan_Sokolov <58785365+dev-sokolov@users.noreply.github.com>
2025-03-01 14:38:29 +0100

    Update App.jsx

--- hw-2/src/components/App/App.jsx
+++ hw-2/src/components/App/App.jsx
@@ -19,8 +19,7 @@ function App() {
       <Greeting name="Егор"/>
       <ShoppingList items={goods}/>
       <ShoppingList items={goodsEmpty}/>
-      <OrderStatus info={orderInfo}/>
-      {/* cheng */}
+      <OrderStatus info={orderInfo}/>     
     </>
   );
 }
